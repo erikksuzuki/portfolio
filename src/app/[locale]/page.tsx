2279be8170a0bcfaa116028e9876a460881798c6
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-01 23:52:31 -0700

    Ascii portrait

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import WorkBlockscope from '@/components/WorkBlockscope/WorkBlockscope'
 import WorkFreelance from '@/components/WorkFreelance'
 import Footer from '@/components/Footer/Footer'
 import SocialLinks from '@/components/Footer/SocialLinks'
+import AsciiPortrait from '@/components/AsciiPortrait'
 
 // https://www.youtube.com/watch?v=PeFqGrEHnp0
 
@@ -11,13 +12,11 @@ export default function Home() {
   return (
     <div>
       <main className="bg-black">
-        <section className="text-left grid grid-cols-1 py-24 px-4 md:px-8 w-full mx-auto max-w-[1024px]">
-          <div>
+        <section className="text-left gap-y-6 grid grid-cols-1 md:grid-cols-2 py-24 px-4 md:px-8 w-full mx-auto max-w-[1024px]">
+          <div className="md:order-1 order-2">
             <h1 className="text-theme-xs">Hi, my name is</h1>
-            <h1 className="text-theme-heading-xs font-poppins mb-3">
-              Eric Suzuki
-            </h1>
-            <p className="text-theme-sm font-exo">
+            <h1 className="text-theme-heading-sm font-exo mb-3">Eric Suzuki</h1>
+            <p className="text-theme-sm font-exo opacity-[0.85]">
               I&apos;m a Product Engineer experienced with complex
               Software-as-a-Service platforms, specializing in front-end
               architecture and building pixel-perfect, customized UI component
@@ -45,6 +44,9 @@ export default function Home() {
               </ul>
             </div>
             <SocialLinks className="mt-4" />
+          </div>
+          <div className="md:order-2 order-1 flex justify-center items-center">
+            <AsciiPortrait />
           </div>
         </section>
         {/* 
