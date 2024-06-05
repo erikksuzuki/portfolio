b32cfebbdab77224b2e369068264e9b6f3217bd2
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-04 18:05:54 -0700

    Refactor social link component into pure component

--- src/components/Footer/SocialLinks.tsx
+++ src/components/Footer/SocialLinks.tsx
@@ -1,15 +1,13 @@
-import IconGithub from '@/assets/icons/common/IconGithub'
-import IconInstagram from '@/assets/icons/common/IconInstagram'
-import IconLinkedin from '@/assets/icons/common/IconLinkedin'
-import IconTumblr from '@/assets/icons/common/IconTumblr'
 import Link from 'next/link'
 import clsx from 'clsx'
-import IconCv from '@/assets/icons/common/IconCv'
+import { SocialLink } from '@/app/socialLinkData'
 
 const SocialLinks = ({
+  data,
   className,
   label = 'Find me on:',
 }: {
+  data: SocialLink[]
   className?: string
   label?: string
 }) => {
@@ -17,27 +15,13 @@ const SocialLinks = ({
     <section className={clsx(className, 'flex flex-col gap-4')}>
       <p className="text-theme-sm">{label}</p>
       <div className="flex flex-row gap-6 items-end">
-        <Link target="_blank" href="https://github.com/erikksuzuki/portfolio">
-          <IconGithub className="w-6 h-6" />
-        </Link>
-        <Link target="_blank" href="https://www.instagram.com/erikksuzuki">
-          <IconInstagram className="w-6 h-6" />
-        </Link>
-        <Link
-          target="_blank"
-          href="https://www.linkedin.com/in/eric-suzuki-04380449/"
-        >
-          <IconLinkedin className="w-6 h-6" />
-        </Link>
-        <Link target="_blank" href="https://emptyblueprints-blog.tumblr.com">
-          <IconTumblr className="w-6 h-6" />
-        </Link>
-        <Link
-          target="_blank"
-          href="https://archived.alkemyst.app/eric_suzuki_cv_2024_lean.pdf"
-        >
-          <IconCv className="w-6 h-6" />
-        </Link>
+        {data.map((link: SocialLink) => {
+          return (
+            <Link key={link.href} target="_blank" href={link.href}>
+              {link.icon}
+            </Link>
+          )
+        })}
       </div>
     </section>
   )
