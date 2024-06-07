a5bf374f418369e53b1ad8957f25c9308aa28722
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-06 17:57:54 -0700

    Add mouseovers to social links

--- src/app/socialLinkData.tsx
+++ src/app/socialLinkData.tsx
@@ -16,31 +16,31 @@ export const socialLinkData: SocialLink[] = [
   {
     name: 'GitHub',
     href: 'https://github.com/erikksuzuki/portfolio',
-    icon: <IconGithub className="w-6 h-6" />,
+    icon: <IconGithub className="w-6 h-6 hover:text-[#DDDDDD]" />,
   },
   {
     name: 'Instagram',
     href: 'https://www.instagram.com/erikksuzuki',
-    icon: <IconInstagram className="w-6 h-6" />,
+    icon: <IconInstagram className="w-6 h-6 hover:text-[#FAA6A0]" />,
   },
   {
     name: 'LinkedIn',
     href: 'https://www.linkedin.com/in/eric-suzuki-04380449/',
-    icon: <IconLinkedin className="w-6 h-6" />,
+    icon: <IconLinkedin className="w-6 h-6 hover:text-[#4DA6F0]" />,
   },
   {
     name: 'Tumblr',
     href: 'https://emptyblueprints-blog.tumblr.com',
-    icon: <IconTumblr className="w-6 h-6" />,
+    icon: <IconTumblr className="w-6 h-6 hover:text-[#4DCAE0]" />,
   },
   {
     name: 'Resume',
     href: 'https://archived.alkemyst.app/eric_suzuki_cv_2024_with_portfolio.pdf',
-    icon: <IconCv className="w-6 h-6" />,
+    icon: <IconCv className="w-6 h-6 hover:text-[#DDDDDD]" />,
   },
   {
     name: 'Calendly',
     href: 'https://calendly.com/erikksuzuki/one-hour-block',
-    icon: <IconCalendly className="w-6 h-6" />,
+    icon: <IconCalendly className="w-6 h-6 hover:text-[#DDDDDD]" />,
   },
 ]
