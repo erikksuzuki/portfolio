b36a03099ecc42721fac29bae3eef68c7b26299e
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-24 21:22:14 -0700

    Add links to artists

--- src/components/SpotifySection/TopArtists.tsx
+++ src/components/SpotifySection/TopArtists.tsx
@@ -1,24 +1,27 @@
+import Link from 'next/link'
+
 const TopArtistsList = ({ data }: any) => {
   return (
-    <ul className="grid grid-cols-2 gap-x-4 text-theme-sm">
+    <ul className="grid grid-cols-2 gap-x-4 md:gap-x-8 text-theme-sm">
       {data.slice(0, 28).map((artist: any, index: number) => {
         return (
-          <li
+          <Link
             key={artist.name + index + 'list'}
-            className="flex items-center gap-2 mb-1"
+            href={artist.external_urls.spotify}
+            target="_blank"
           >
-            <div
-              style={{
-                backgroundImage: `url(${artist.images[1].url})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-              }}
-              className="rounded-full inline-block w-[16px] h-[16px] border border-[rgba(255,255,255,0.3)]"
-            />
-            <span className=" bg-acryllic-black opacity-[0.75]">
-              {artist.name}
-            </span>
-          </li>
+            <li className="flex items-center gap-2 mb-1">
+              <div
+                style={{
+                  backgroundImage: `url(${artist.images[1].url})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center center',
+                }}
+                className="rounded-full inline-block w-[16px] h-[16px] border border-[rgba(255,255,255,0.3)]"
+              />
+              <span className="opacity-[0.75]">{artist.name}</span>
+            </li>
+          </Link>
         )
       })}
     </ul>
