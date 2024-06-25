892f560dff3cf0cf634fa514ae905b2b0d4dbbda
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-24 22:55:59 -0700

    Add audio wave loading spinner for spotify

---
+++ src/components/SpotifySection/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+const SpotifyLoadingSpinner = () => {
+  return (
+    <div className="spotify-loading-spinner">
+      <div className="spotify-loading spotify-loading1"></div>
+      <div className="spotify-loading spotify-loading2"></div>
+      <div className="spotify-loading spotify-loading3"></div>
+      <div className="spotify-loading spotify-loading4"></div>
+      <div className="spotify-loading spotify-loading5"></div>
+    </div>
+  )
+}
+
+export default SpotifyLoadingSpinner
