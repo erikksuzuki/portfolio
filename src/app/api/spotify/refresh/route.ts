6b5572192e54c1655295a486bd95317e54d16fee
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-24 21:10:06 -0700

    Add Spotify API functions to the backend

---
+++ src/app/api/spotify/refresh/route.ts
@@ -0,0 +1,72 @@
+import { createClient } from '@/utils/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function refreshApiToken(refreshToken: string) {
+  // format the post body
+  const details: any = {
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken,
+    client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+  }
+  let params = []
+  for (let property in details) {
+    const encodedKey = encodeURIComponent(property)
+    const encodedValue = encodeURIComponent(details[property])
+    params.push(encodedKey + '=' + encodedValue)
+  }
+  const formBody = params.join('&')
+
+  // make the fetch request with formatted post body
+  let refreshError: string | undefined = undefined
+  const response = await fetch(`https://accounts.spotify.com/api/token`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Basic ${process.env.SPOTIFY_AUTHORIZATION_STRING}=`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: formBody,
+    cache: 'no-cache',
+  })
+    .then((result) => result.json())
+    .then((data) => data)
+    .catch(() => {
+      refreshError = 'An error occured when refreshing token'
+    })
+
+  // function to calculate the time of token expiration
+  function oneHourFromCreation(date: Date) {
+    const addedTime = 3600000
+    date.setTime(date.getTime() + addedTime)
+    return date
+  }
+
+  // update database with new tokens and their timestamps
+  await createClient()
+    .from('spotify')
+    .update(
+      !response.refresh_token
+        ? {
+            access_token: response.access_token,
+            created_at: new Date(),
+            expires_at: oneHourFromCreation(new Date()),
+          }
+        : {
+            access_token: response.access_token,
+            created_at: new Date(),
+            expires_at: oneHourFromCreation(new Date()),
+            refresh_token: response.refresh_token,
+          }
+    )
+    .eq('id', '1')
+    .select()
+}
+
+export async function GET() {
+  const { data: existingTokens } = await createClient()
+    .from('spotify')
+    .select('*')
+    .single()
+  await refreshApiToken(existingTokens.refresh_token)
+  return NextResponse.json({ data: 'Access token refresh invoked' })
+}
---
+++ src/app/authService.tsx
@@ -0,0 +1,42 @@
+const getHeaders = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': `application/json`,
+  },
+}
+
+type SpotifyTimeRange = 'short_term' | 'medium_term' | 'long_term'
+export async function getSpotifyTopArtists(
+  limit: number = 48,
+  timeRange: SpotifyTimeRange = 'medium_term'
+) {
+  try {
+    const response = await fetch(
+      `/api/spotify?endpoint=top/artists&limit=${limit}&time_range=${timeRange}`,
+      getHeaders
+    )
+    if (response) return await response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+export async function getSpotifyRecentlyPlayed(limit: number = 10) {
+  try {
+    const response = await fetch(
+      `/api/spotify?endpoint=player/recently-played&limit=${limit}`,
+      getHeaders
+    )
+    if (response) return await response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+export async function getSpotifyPlayingNow() {
+  try {
+    const response = await fetch(`/api/spotify?endpoint=player`, getHeaders)
+    if (response) return await response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
