7aaad1f1a6a952bce0ba8fcee177835fa3c04aa7
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-25 18:00:49 -0700

    string truncation function

---
+++ src/utils/formatString.ts
@@ -0,0 +1,13 @@
+export function truncateParagraph(
+  str: string,
+  n: number,
+  useWordBoundary: boolean
+) {
+  if (str.length <= n) return str
+  const subString = str.slice(0, n - 1) // the original check
+  return (
+    (useWordBoundary
+      ? subString.slice(0, subString.lastIndexOf(' '))
+      : subString) + '...'
+  )
+}
