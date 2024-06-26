47ce3e81773d7805f47c731b6e3493c2338ae1e0
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-06-26 09:02:37 -0700

    Update RecentTracks.tsx

--- src/components/SpotifySection/RecentTracks.tsx
+++ src/components/SpotifySection/RecentTracks.tsx
@@ -38,7 +38,7 @@ const SpotifyRecentTracks = ({ playingNow, recentTracks }: any) => {
                 lastItem={playingNow ? index === 5 : index === 6}
               />
             ))}
-        {(!playingNow || !recentTracks) && (
+        {!playingNow && !recentTracks && (
           <div className="text-white flex flex-col justify-center items-center opacity-[0.4]">
             <SpotifyLoadingSpinner />
             <span className="text-theme-xs mt-4 mb-12">Loading...</span>
