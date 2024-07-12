b6bc03a43f5b5c9ea5f631e9362128e94f83c117
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-07-12 05:25:20 -0700

    Add track listing tooltip to Spotify

--- src/components/SpotifySection/TrackListing.tsx
+++ src/components/SpotifySection/TrackListing.tsx
@@ -10,6 +10,8 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import { truncateParagraph } from '@/utils/formatString'
 import { useResponsive } from '@/hooks/useResponsive'
+import * as Tooltip from '@radix-ui/react-tooltip'
+import IconExternalPage from '@/assets/icons/common/IconExternalPage'
 
 const SpotifyTrackListing = ({ track, lastItem }: any) => {
   const { isAboveMd } = useResponsive('md')
@@ -24,42 +26,67 @@ const SpotifyTrackListing = ({ track, lastItem }: any) => {
   const attributions = track.track.artists
     .map((artist: any) => artist.name)
     .join(', ')
+
+  const listItem = (
+    <li
+      className={clsx('flex items-center justify-between w-full', {
+        'border-b border-[rgba(255,255,255,0.1)] mb-2 pb-2': !lastItem,
+      })}
+    >
+      <div className="flex items-center gap-x-4">
+        <div className="min-w-[36px] min-h-[36px] overflow-hidden rounded-sm border border-[rgba(255,255,255,0.2)]">
+          <Image
+            alt={track.track.album.name}
+            src={track.track.album.images[0].url}
+            height={36}
+            width={36}
+          />
+        </div>
+        <div>
+          <p className="text-theme-sm">
+            {truncateParagraph(track.track.name, isAboveMd ? 39 : 26, false)}
+          </p>
+          <p className="text-theme-xs opacity-[0.8]">{attributions}</p>
+        </div>
+      </div>
+      <div className="text-right min-w-[100px]">
+        <p className="text-theme-xs opacity-[0.7] mb-[2px]">
+          {formatAMPM(track.played_at)}
+        </p>
+        <p className="text-theme-xs opacity-[0.5]">
+          {timeAgo.format(playedAt)}
+        </p>
+      </div>
+    </li>
+  )
   return (
     <Link
       href={track.track.external_urls.spotify}
       target="_blank"
       className="w-full"
     >
-      <li
-        className={clsx('flex items-center justify-between w-full', {
-          'border-b border-[rgba(255,255,255,0.1)] mb-2 pb-2': !lastItem,
-        })}
-      >
-        <div className="flex items-center gap-x-4">
-          <div className="min-w-[36px] min-h-[36px] overflow-hidden rounded-sm border border-[rgba(255,255,255,0.2)]">
-            <Image
-              alt={track.track.album.name}
-              src={track.track.album.images[0].url}
-              height={36}
-              width={36}
-            />
-          </div>
-          <div>
-            <p className="text-theme-sm">
-              {truncateParagraph(track.track.name, isAboveMd ? 39 : 26, false)}
-            </p>
-            <p className="text-theme-xs opacity-[0.8]">{attributions}</p>
-          </div>
-        </div>
-        <div className="text-right min-w-[100px]">
-          <p className="text-theme-xs opacity-[0.7] mb-[2px]">
-            {formatAMPM(track.played_at)}
-          </p>
-          <p className="text-theme-xs opacity-[0.5]">
-            {timeAgo.format(playedAt)}
-          </p>
-        </div>
-      </li>
+      {isAboveMd ? (
+        <Tooltip.Provider delayDuration={0}>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>{listItem}</Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content
+                className="TooltipContent"
+                sideOffset={5}
+                side="right"
+              >
+                <div className="flex flex-row justify-center items-center gap-x-2">
+                  <h2 className="text-theme-sm">View in Spotify</h2>
+                  <IconExternalPage className="w-4 h-4" />
+                </div>
+                <Tooltip.Arrow className="TooltipArrow" />
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
+      ) : (
+        listItem
+      )}
     </Link>
   )
 }
