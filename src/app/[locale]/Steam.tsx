431977a561c364b06b7cbf8bfc55f924d3249170
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-07-12 13:36:31 -0700

    Add backdrop filter to steam backgrounds

--- src/app/[locale]/Steam.tsx
+++ src/app/[locale]/Steam.tsx
@@ -50,109 +50,112 @@ const SteamSection = () => {
             ? SteamLoadingBackground.src
             : gameData.details.backgrounds[0]
         })`,
+        backgroundColor: '#080E3D',
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
       }}
       className="w-full h-[720px] bg-[#000] border-white overflow-hidden relative"
     >
-      <section className="text-left gap-y-6 grid grid-cols-1 md:grid-cols-2 py-24 px-0 md:px-4 w-full mx-auto max-w-[1024px] relative">
-        <SteamRecentGames
-          recentlyPlayed={playerData?.data?.recently_played}
-          setGameData={getAndSetGameData}
-        />
-        <aside className="w-full hidden md:flex items-center justify-center bg-acryllic-blacker rounded-lg border border-[rgba(255,255,255,0.1)]">
-          <div className="md:px-8 px-4 py-8 w-full max-w-[460px]">
-            <div className="flex flex-row justify-between items-end mb-3">
-              <h2 className="text-theme-sm capitalize">
-                {gameData?.details.name.toLowerCase()}
-              </h2>
-              <p className="text-theme-xs">Released {releaseDateFormatted}</p>
-            </div>
-            <p className="text-theme-xs mb-3">
-              {gameData?.details.short_description}
-            </p>
-            <p className="text-theme-xs mb-3">
-              Total playtime since account creation:
-              <br />
-              {humanizeDuration(
-                playerData?.data?.recently_played.filter((game: any) => {
-                  return Number(game?.appid) === Number(gameData?.appid)
-                })[0]?.playtime_forever * 60000,
-                { units: ['h', 'm', 's', 'ms'] }
-              )}
-            </p>
-            <ul className="">
-              {gameData?.achievements?.unlockedAchievements &&
-                gameData?.achievements?.unlockedAchievements
-                  .slice(0, 24)
-                  .map((achievement: any) => {
-                    return (
-                      <Tooltip.Provider
-                        key={achievement.name}
-                        delayDuration={0}
-                      >
-                        <Tooltip.Root>
-                          <Tooltip.Trigger asChild>
-                            <li className="border border-[rgba(255,155,0,0.6)] w-[24px] h-[24px] inline-block mr-2">
-                              <Image
-                                width={24}
-                                height={24}
-                                alt={achievement.display_name}
-                                src={achievement.icon}
-                              />
-                            </li>
-                          </Tooltip.Trigger>
-                          <Tooltip.Portal>
-                            <Tooltip.Content
-                              className="TooltipContent"
-                              sideOffset={5}
-                            >
-                              <div className="max-w-[280px]">
-                                <h2 className="text-theme-sm text-[rgb(255,195,0)]">
-                                  {achievement.display_name}
-                                </h2>
-                                <p
-                                  className={clsx('text-theme-xs mb-3', {
-                                    'italic text-[rgba(255,255,255,0.6)]':
-                                      !achievement.description,
-                                  })}
-                                >
-                                  {achievement.description ??
-                                    'Hidden achievement'}
-                                </p>
-                                <p className="text-theme-xs">
-                                  Unlocked:{' '}
-                                  {formatDateOrdinal(
-                                    achievement.unlocktime,
-                                    true,
-                                    true
-                                  )}
-                                </p>
-                              </div>
-                              <Tooltip.Arrow className="TooltipArrow" />
-                            </Tooltip.Content>
-                          </Tooltip.Portal>
-                        </Tooltip.Root>
-                      </Tooltip.Provider>
-                    )
-                  })}
-            </ul>
-            {gameData?.achievements?.unlockedAchievements && (
-              <p className="text-theme-xs mb-2">
-                {`${gameData?.achievements?.unlockedAchievementCount} / ${gameData?.achievements?.maxAchievementCount}`}{' '}
-                achievements unlocked
+      <div className="bg-steam-backdrop">
+        <section className="text-left gap-y-6 grid grid-cols-1 md:grid-cols-2 py-24 px-0 md:px-4 w-full mx-auto max-w-[1024px] relative">
+          <SteamRecentGames
+            recentlyPlayed={playerData?.data?.recently_played}
+            setGameData={getAndSetGameData}
+          />
+          <aside className="w-full hidden md:flex items-center justify-center bg-acryllic-blacker rounded-lg border border-[rgba(255,255,255,0.1)] relative">
+            <div className="md:px-8 px-4 py-8 w-full max-w-[460px]">
+              <div className="flex flex-row justify-between items-end mb-3">
+                <h2 className="text-theme-sm capitalize">
+                  {gameData?.details.name.toLowerCase()}
+                </h2>
+                <p className="text-theme-xs">Released {releaseDateFormatted}</p>
+              </div>
+              <p className="text-theme-xs mb-3">
+                {gameData?.details.short_description}
               </p>
-            )}
-            <Link
-              className="text-theme-xs flex items-center justify-start gap-x-2 text-[rgba(255,255,255,0.8)] hover:text-[rgba(0,195,255)]"
-              href={`https://store.steampowered.com/app/${gameData?.appid}/`}
-              target="_blank"
-            >
-              Visit Steam store page <IconExternalPage className="w-3 h-3" />
-            </Link>
-          </div>
-        </aside>
-      </section>
+              <p className="text-theme-xs mb-3">
+                Total playtime since account creation:
+                <br />
+                {humanizeDuration(
+                  playerData?.data?.recently_played.filter((game: any) => {
+                    return Number(game?.appid) === Number(gameData?.appid)
+                  })[0]?.playtime_forever * 60000,
+                  { units: ['h', 'm', 's', 'ms'] }
+                )}
+              </p>
+              <ul className="">
+                {gameData?.achievements?.unlockedAchievements &&
+                  gameData?.achievements?.unlockedAchievements
+                    .slice(0, 24)
+                    .map((achievement: any) => {
+                      return (
+                        <Tooltip.Provider
+                          key={achievement.name}
+                          delayDuration={0}
+                        >
+                          <Tooltip.Root>
+                            <Tooltip.Trigger asChild>
+                              <li className="border border-[rgba(255,155,0,0.6)] w-[24px] h-[24px] inline-block mr-2">
+                                <Image
+                                  width={24}
+                                  height={24}
+                                  alt={achievement.display_name}
+                                  src={achievement.icon}
+                                />
+                              </li>
+                            </Tooltip.Trigger>
+                            <Tooltip.Portal>
+                              <Tooltip.Content
+                                className="TooltipContent"
+                                sideOffset={5}
+                              >
+                                <div className="max-w-[280px]">
+                                  <h2 className="text-theme-sm text-[rgb(255,195,0)]">
+                                    {achievement.display_name}
+                                  </h2>
+                                  <p
+                                    className={clsx('text-theme-xs mb-3', {
+                                      'italic text-[rgba(255,255,255,0.6)]':
+                                        !achievement.description,
+                                    })}
+                                  >
+                                    {achievement.description ??
+                                      'Hidden achievement'}
+                                  </p>
+                                  <p className="text-theme-xs">
+                                    Unlocked:{' '}
+                                    {formatDateOrdinal(
+                                      achievement.unlocktime,
+                                      true,
+                                      true
+                                    )}
+                                  </p>
+                                </div>
+                                <Tooltip.Arrow className="TooltipArrow" />
+                              </Tooltip.Content>
+                            </Tooltip.Portal>
+                          </Tooltip.Root>
+                        </Tooltip.Provider>
+                      )
+                    })}
+              </ul>
+              {gameData?.achievements?.unlockedAchievements && (
+                <p className="text-theme-xs mb-2">
+                  {`${gameData?.achievements?.unlockedAchievementCount} / ${gameData?.achievements?.maxAchievementCount}`}{' '}
+                  achievements unlocked
+                </p>
+              )}
+              <Link
+                className="text-theme-xs flex items-center justify-start gap-x-2 text-[rgba(0,195,255)] hover:text-[rgba(255,255,255,0.8)]"
+                href={`https://store.steampowered.com/app/${gameData?.appid}/`}
+                target="_blank"
+              >
+                Visit Steam store page <IconExternalPage className="w-3 h-3" />
+              </Link>
+            </div>
+          </aside>
+        </section>
+      </div>
     </section>
   )
 }
