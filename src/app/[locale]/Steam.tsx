241c01d39bed465af682b128ded14a2261be3ac3
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-07-12 03:23:02 -0700

    Add achievements section

--- src/app/[locale]/Steam.tsx
+++ src/app/[locale]/Steam.tsx
@@ -4,9 +4,11 @@ import { useEffect, useState } from 'react'
 import { getSteamGameInfo, getSteamPlayerInfo } from '@/app/authService'
 import SteamRecentGames from '@/components/SteamSection/RecentGames'
 import { formatDateOrdinal } from '@/utils/formatDateTime'
+import Image from 'next/image'
 // import jp from 'javascript-time-ago/locale/jp'
 
 const SteamSection = () => {
+  const humanizeDuration = require('humanize-duration')
   const [playerData, setPlayerData] = useState<any>()
   const [gameData, setGameData] = useState<any>()
 
@@ -26,7 +28,7 @@ const SteamSection = () => {
     playerData && getAndSetGameData(playerData?.data?.recently_played[0].appid)
   }, [playerData])
 
-  const bgOverrides = ['690040']
+  const bgOverrides = [690040]
   const overrideBg = bgOverrides.includes(gameData?.appid) ? true : false
   const releaseDateFormatted = gameData
     ? formatDateOrdinal(gameData.details.release_date, true, true)
@@ -48,14 +50,47 @@ const SteamSection = () => {
           recentlyPlayed={playerData?.data?.recently_played}
           setGameData={getAndSetGameData}
         />
-        <aside className="w-full hidden md:flex items-center justify-center bg-acryllic-black rounded-lg border border-[rgba(255,255,255,0.1)]">
+        <aside className="w-full hidden md:flex items-center justify-center bg-acryllic-blacker rounded-lg border border-[rgba(255,255,255,0.1)]">
           <div className="md:px-8 px-4 py-8 w-full max-w-[460px]">
             <div className="flex flex-row justify-between items-end mb-3">
               <h2 className="text-theme-sm">{gameData?.details.name}</h2>
               <p className="text-theme-xs">Released {releaseDateFormatted}</p>
             </div>
-            <p className="text-theme-xs">
+            <p className="text-theme-xs mb-3">
               {gameData?.details.short_description}
+            </p>
+            <p className="text-theme-xs mb-3">
+              Active app time for the past two weeks:
+              <br />
+              {humanizeDuration(
+                playerData?.data?.recently_played.filter((game: any) => {
+                  return Number(game?.appid) === Number(gameData?.appid)
+                })[0]?.playtime_2weeks * 60000,
+                { units: ['h', 'm', 's', 'ms'] }
+              )}
+            </p>
+            <ul className="">
+              {gameData?.achievements?.unlockedAchievements
+                .slice(0, 24)
+                .map((achievement: any) => {
+                  return (
+                    <li
+                      className="border border-[rgba(255,155,0,0.6)] w-[24px] h-[24px] inline-block mr-2"
+                      key={achievement.name}
+                    >
+                      <Image
+                        width={24}
+                        height={24}
+                        alt={achievement.display_name}
+                        src={achievement.icon}
+                      />
+                    </li>
+                  )
+                })}
+            </ul>
+            <p className="text-theme-xs">
+              {`${gameData?.achievements?.unlockedAchievementCount} / ${gameData?.achievements?.maxAchievementCount}`}{' '}
+              achievements unlocked
             </p>
           </div>
         </aside>
