37d04581e6574f6dc5eea2945904999d9f1bb1f4
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-08-06 22:27:49 -0700

    Add comments and optimizations to Steam API

--- src/app/api/steam/game-details/route.ts
+++ src/app/api/steam/game-details/route.ts
@@ -9,33 +9,34 @@ export async function GET(request: NextRequest) {
   const searchParams = new URL(request.url).searchParams
   const appid: any = searchParams.get('appid')
 
+  // fetch original server data
   const gameDetails: any = await getGameDetails(appid)
   const gameStats: any = await getPlayerGameStats(appid)
   const gameAchievements: any = await getPlayerGameAchievements(appid)
   const gameSchema: any = await getGameSchema(appid)
+  const backgrounds = [gameDetails[appid].data.background]
+  const capsule_images = [gameDetails[appid].data.capsule_image]
 
-  // parse game details
+  // create details object
   let details: any = {}
+  // parse game details
   if (gameDetails && gameStats && gameAchievements && gameSchema) {
     const genres = gameDetails[appid].data.genres.map(
       (genre: any) => genre.description
     )
-    const backgrounds = [gameDetails[appid].data.background]
+    // if additional backgrounds or capsule images exist, add them to array
     gameDetails[appid].data.background_raw &&
       backgrounds.push(gameDetails[appid].data.background_raw)
-    const capsule_images = [gameDetails[appid].data.capsule_image]
     gameDetails[appid].data.capsule_imagev5 &&
       capsule_images.push(gameDetails[appid].data.capsule_imagev5)
+    // construct details object
     details = {
       name: gameDetails[appid].data.name,
       short_description: gameDetails[appid].data.short_description,
       release_date: new Date(gameDetails[appid].data.release_date.date),
       website: gameDetails[appid].data.website,
-
       developers: gameDetails[appid].data.developers,
-
       publishers: gameDetails[appid].data.publishers,
-
       genres,
       backgrounds,
       capsule_images,
@@ -76,15 +77,21 @@ export async function GET(request: NextRequest) {
     gameSchema &&
     gameAchievements.playerstats?.achievements
   ) {
-    const maxAchievementCount = gameAchievements.playerstats.achievements.length
-    const unlockedAchievementCount =
+    // get an array of all earned player achievements for the game
+    const unlockedAchievements =
       gameAchievements.playerstats.achievements.filter(
         (achievement: any) => achievement.achieved === 1
-      ).length
-    const achievementData = gameAchievements.playerstats.achievements
-      .filter((achievement: any) => achievement.achieved === 1)
-      .sort((a: any, b: any) => (a.unlocktime > b.unlocktime ? -1 : 1))
+      )
+    // counts for earned vs possible achievements
+    const unlockedAchievementCount = unlockedAchievements.length
+    const maxAchievementCount = gameAchievements.playerstats.achievements.length
+    // get and sort list of earned player achievements for the game
+    const achievementData = unlockedAchievements.sort((a: any, b: any) =>
+      a.unlocktime > b.unlocktime ? -1 : 1
+    )
+    // retrieve all available achievements for the game (schema object)
     const achievementSchema = gameSchema.game.availableGameStats.achievements
+    // for each earned player achievement, scrape data from schema object
     const achievementArray = achievementData.map((achievement: any) => {
       const achievementName = achievementSchema.filter(
         (achievementSchema: any) =>
