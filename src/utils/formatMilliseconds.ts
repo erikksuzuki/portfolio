e233dd858a94025cd1a897b6dd25e377c7e63be9
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-09-22 23:02:56 -0700

    update format miliseconds utility function

--- src/utils/formatMilliseconds.ts
+++ src/utils/formatMilliseconds.ts
@@ -1,8 +1,21 @@
 // takes miliseconds as input and returns formatted minutes and seconds as a string
 // does not return hours yet
 
-export function formatMilliseconds(ms: number): string {
-  let minutes = Math.floor(ms / 60000)
+export function formatMilliseconds(ms: number, denoted?: boolean): string {
+  let hours = Math.floor(ms / 3600000)
+  let minutesRemaining = Math.floor(ms / 60000) - hours * 60
+  let minutesOnly = Math.floor(ms / 60000)
   let seconds: number = Number(((ms % 60000) / 1000).toFixed(0))
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+  let conditionalHours = hours === 0 ? '' : `${hours}h `
+
+  if (denoted)
+    return (
+      conditionalHours +
+      minutesRemaining +
+      'm ' +
+      (seconds < 10 ? '0' : '') +
+      seconds +
+      's'
+    )
+  return minutesOnly + ':' + (seconds < 10 ? '0' : '') + seconds
 }
