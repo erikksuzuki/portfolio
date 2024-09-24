6c5164a99044aa68e54ea246e91360521771483b
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-09-23 20:19:46 -0700

    Update page.tsx

--- src/app/[locale]/codinginterview/page.tsx
+++ src/app/[locale]/codinginterview/page.tsx
@@ -2,6 +2,9 @@
 
 import { getTemperatureFromSmile } from '@/app/authService'
 import { useState } from 'react'
+import ChemicalMolecules from '@/assets/coding-interview/reaction.png'
+import Image from 'next/image'
+import clsx from 'clsx'
 
 export interface HistoryObject {
   input: string | null
@@ -9,7 +12,9 @@ export interface HistoryObject {
 }
 
 const CodingIntervieewPage = () => {
-  const [inputValue, setInputValue] = useState<string>('')
+  const [inputValue, setInputValue] = useState<string>(
+    'CN(C)c1cccc([N+](=O)[O-])c1C#N.CO.Cl.[Fe]>>CN(C)c1cccc(N)c1C#N'
+  )
   const [resultValue, setResultValue] = useState<number>(0)
   const [valueHistory, setValueHistory] = useState<HistoryObject[]>([])
 
@@ -27,29 +32,60 @@ const CodingIntervieewPage = () => {
   }
   return (
     <section className="text-left gap-y-6 py-24 px-4 md:px-8 w-full mx-auto max-w-[1024px] relative">
-      <div className="flex gap-x-4">
-        Input SMILE:
-        <form action={handleSubmit}>
-          <input
-            className="text-black px-2 py-1"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button type="submit" className="ml-2">
-            Submit
-          </button>
-        </form>
-      </div>
-      <div>Result: {`${resultValue}º`}</div>
-      <div>
-        {valueHistory.map((historyItem: HistoryObject, index: number) => {
-          return (
-            <div className="grid grid-cols-2" key={index}>
-              <div>SMILE: {historyItem.input}</div>
-              <div>Temp: {historyItem.value}º</div>
+      <h1 className="text-center text-theme-heading-sm mb-10">
+        Chemical Reaction Temperature Calculator
+      </h1>
+      <div className="grid grid-cols-2 gap-x-8">
+        <div>
+          <form action={handleSubmit} className="flex flex-col gap-x-4 mb-8">
+            <label className="text-theme-sm mb-1">
+              Input reaction in SMILES format
+            </label>
+            <div className="flex flex-row gap-2">
+              <input
+                className="text-black px-2 py-1 rounded-sm border border-[rgba(255,255,255,0.4)]"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="ml-2 px-2 py-1 rounded-sm bg-[rgba(0,0,255,0.4)] border border-[rgba(255,255,255,0.4)]"
+              >
+                Calculate
+              </button>
             </div>
-          )
-        })}
+          </form>
+          <figure className="px-3 pt-16 pb-6 border border-white rounded-xl">
+            <Image
+              alt="Chemical Reaction"
+              src={ChemicalMolecules.src}
+              width={363}
+              height={137}
+              className="w-full"
+            />
+            <h4 className="mt-4 text-theme-xs text-center">{inputValue}</h4>
+          </figure>
+        </div>
+        <div>
+          <div className="text-center py-6 text-theme-heading-md">{`${resultValue}º`}</div>
+          <div>
+            {valueHistory.map((historyItem: HistoryObject, index: number) => {
+              const lastItem = index === valueHistory.length - 1
+              return (
+                <div
+                  className={clsx(
+                    'flex flex-row justify-between gap-x-[100px] items-center py-4 px-2 border-white',
+                    { 'border-b': !lastItem }
+                  )}
+                  key={index}
+                >
+                  <div className="text-theme-xs">{historyItem.input}</div>
+                  <div>{historyItem.value}º</div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     </section>
   )
