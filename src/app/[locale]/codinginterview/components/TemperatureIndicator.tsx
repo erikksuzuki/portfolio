926112c9bc404ac42857d1522b14715367b4a3e5
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-09-23 21:01:06 -0700

    label and improve temperature indication appearance

--- src/app/[locale]/codinginterview/components/TemperatureIndicator.tsx
+++ src/app/[locale]/codinginterview/components/TemperatureIndicator.tsx
@@ -1,6 +1,11 @@
 const TemperatureIndicator = ({ temp }: { temp: number }) => {
   return (
-    <figure className="text-center py-6 text-theme-heading-md">{`${temp}º`}</figure>
+    <article className="flex flex-col items-center justify-center bg-[rgba(0,0,255,0.4)] rounded-xl">
+      <figure className="text-center pt-6 text-theme-heading-md">{`${temp}º`}</figure>
+      <label className="mt-1 pb-6 text-theme-xs text-center w-full">
+        resulting temperature
+      </label>
+    </article>
   )
 }
 
