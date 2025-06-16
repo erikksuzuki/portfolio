8ec9e231c4117598fe7057b0a2d4f81c31a498f0
Eric Suzuki <erikksuzuki@gmail.com>
2025-06-16 22:32:26 +0700

    Update route.ts

--- src/app/api/igdb/route.ts
+++ src/app/api/igdb/route.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@/utils/supabase/server'
 import { NextRequest, NextResponse } from 'next/server'
-// import { refreshIGDBToken } from './refreshIGDBToken'
+import { refreshIGDBToken } from './refreshIGDBToken'
 
 export async function GET(request: NextRequest) {
   const searchParams = new URL(request.url).searchParams
@@ -14,43 +14,33 @@ export async function GET(request: NextRequest) {
     ? new Date(existingTokenData.expires_at).getTime()
     : new Date().getTime() - 3600000 // Default to 1 hour if no expiration time is set
   const tokenHasExpired: boolean = now > expiredTime
-  //
-  // async function fetchGames(accessToken: string) {
-  //   const res = await fetch('https://api.igdb.com/v4/games', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Client-ID': process.env.TWITCH_CLIENT_ID ?? '',
-  //       Authorization: `Bearer ${accessToken}`,
-  //       Accept: 'application/json',
-  //       'Content-Type': 'text/plain',
-  //     },
-  //     body: `fields name,cover.url,genres.name; limit 10; search ${query};`,
-  //   })
-  //   const data = await res.json()
-  //   return data
-  // }
-  //
-  // async function getIGDBData() {
-  //   // if valid access token exists, use it to fetch data
-  //   if (!tokenHasExpired) {
-  //     return await fetchGames(existingTokenData.access_token)
-  //     // if access token has expired, refresh the token and use the new one to fetch data
-  //   } else if (tokenHasExpired) {
-  //     const refreshedToken: any = await refreshIGDBToken()
-  //     return await fetchGames(refreshedToken)
-  //   }
-  // }
-  // const igdbData = await getIGDBData()
 
-  const testSupabaseTokenData = {
-    query,
-    message: 'This is a test response from the IGDB API route',
-    existingTokenData,
-    now,
-    expiredTime,
-    tokenHasExpired,
+  async function fetchGames(accessToken: string) {
+    const res = await fetch('https://api.igdb.com/v4/games', {
+      method: 'POST',
+      headers: {
+        'Client-ID': process.env.TWITCH_CLIENT_ID ?? '',
+        Authorization: `Bearer ${accessToken}`,
+        Accept: 'application/json',
+        'Content-Type': 'text/plain',
+      },
+      body: `fields name,cover.url,genres.name; limit 10; search ${query};`,
+    })
+    const data = await res.json()
+    return data
   }
 
-  // return NextResponse.json(igdbData)
-  return NextResponse.json(testSupabaseTokenData)
+  async function getIGDBData() {
+    // if valid access token exists, use it to fetch data
+    if (!tokenHasExpired) {
+      return await fetchGames(existingTokenData.access_token)
+      // if access token has expired, refresh the token and use the new one to fetch data
+    } else if (tokenHasExpired) {
+      const refreshedToken: any = await refreshIGDBToken()
+      return await fetchGames(refreshedToken)
+    }
+  }
+  const igdbData = await getIGDBData()
+
+  return NextResponse.json(igdbData)
 }
