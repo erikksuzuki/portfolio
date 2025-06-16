ab5d5339f141161d0600c6d5e8ca0592b1ea9124
Eric Suzuki <erikksuzuki@gmail.com>
2025-06-16 22:34:20 +0700

    Update route.ts

--- src/app/api/igdb/route.ts
+++ src/app/api/igdb/route.ts
@@ -24,7 +24,7 @@ export async function GET(request: NextRequest) {
         Accept: 'application/json',
         'Content-Type': 'text/plain',
       },
-      body: `fields name,cover.url,genres.name; limit 10; search ${query};`,
+      body: `fields name,cover.url,genres.name; limit 10; search "${query}";`,
     })
     const data = await res.json()
     return data
