8dad12ff71daaf0ec6a7c3ad30188b2f34210984
Eric Suzuki <erikksuzuki@gmail.com>
2025-06-28 05:58:29 -0700

    Update route.ts

--- src/app/api/pdf-to-png-multi/route.ts
+++ src/app/api/pdf-to-png-multi/route.ts
@@ -6,6 +6,15 @@ export const runtime = 'nodejs'
 import { NextRequest, NextResponse } from 'next/server'
 import { randomUUID } from 'crypto'
 
+function streamToString(stream: any) {
+  const chunks: any = []
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk: any) => chunks.push(Buffer.from(chunk)))
+    stream.on('error', (err: any) => reject(err))
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+  })
+}
+
 export async function POST(request: NextRequest) {
   let extractedFilename
   const contentType = request.headers.get('content-type') || ''
@@ -34,7 +43,7 @@ export async function POST(request: NextRequest) {
       JSON.stringify({
         parts: [{ file: 'document' }],
         output: {
-          pages: { start: 0, end: 9 },
+          pages: { start: 0, end: -1 },
           type: 'image',
           format: 'png',
           dpi: 72,
@@ -54,17 +63,16 @@ export async function POST(request: NextRequest) {
           ...uploadForm.getHeaders(),
           Authorization: `Bearer ${process.env.PSPDFKIT_API_KEY}`,
         },
-        responseType: 'arraybuffer',
+        responseType: 'stream',
       }
     )
-
-    // const base64 = Buffer.from(serviceResponse.data).toString('base64')
 
     const response = NextResponse.json({
       filename: extractedFilename,
       sessionId: randomUUID(),
       serviceResponse: serviceResponse,
     })
+
     response.headers.set('Access-Control-Allow-Origin', '*')
     response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS')
     response.headers.set(
@@ -73,7 +81,9 @@ export async function POST(request: NextRequest) {
     )
     return response
   } catch (err: any) {
-    const response = NextResponse.json({ error: err }, { status: 500 })
+    const errorString = await streamToString(err.response.data)
+
+    const response = NextResponse.json({ error: errorString }, { status: 500 })
     response.headers.set('Access-Control-Allow-Origin', '*')
     response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS')
     response.headers.set(
